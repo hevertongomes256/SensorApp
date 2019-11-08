1247f2cbeb4826f53d0f9f93f24bc0f13e6a9bbe
Heverton13 <heverton.gomes2018@gmail.com>
2019-11-07 22:18:34 -0300

    Adição de função para sensor de proximidade

--- app/src/main/java/com/example/sensorapp/ProximidadeActivity.kt
+++ app/src/main/java/com/example/sensorapp/ProximidadeActivity.kt
@@ -14,25 +14,51 @@ import java.io.IOException
 
 class ProximidadeActivity : AppCompatActivity(), SensorEventListener {
 
-    var sensor: Sensor? = null
-    var sensorManager: SensorManager? = null
+    private lateinit
+    var sensorManager: SensorManager
+    private var mProximity: Sensor? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_proximidade)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+    }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
 
 
-    override fun onSensorChanged(event: SensorEvent?) {
+    override fun onSensorChanged(event: SensorEvent) {
+        val distance = event.values[0]
+        textProximidade.text = distance.toString()
 
+        if(distance >= 8){
+            ivMicrofone.setImageResource(R.drawable.fone2)
+        }else{
+            ivMicrofone.setImageResource(R.drawable.fone)
+
+        }
 
     }
 
+    override fun onResume() {
+        super.onResume()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_proximidade)
-        
+        mProximity?.also { proximity ->
+            sensorManager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager.unregisterListener(this)
 
     }
+
 
 }
